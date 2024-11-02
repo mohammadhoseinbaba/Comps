0ee7417cd75366bbb8cf212a39e46fd60d0682dd
MRX <sirhb77@gmail.com>
2024-11-02 22:27:08 +0100

    updated

--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -6,17 +6,22 @@ const [isOpen,setIsopen]=useState(false)
 
 const handleClick = ()=>{
     setIsopen(!isOpen)
+
+    //what option has the user selected
 }
 
-
+const handleOptionClick = ()=>{
+    console.log('i should close')
+    setIsopen(false)
+}
     const renderedDropDown = options.map((option)=>{
         return (
-        <div key={option.value}>
+        <div onClick={handleOptionClick} key={option.value}>
         {option.lable}</div>
         )
     }) 
     return <div>
-        <div onClick={handleClick}>Select...</div>
+        <div  onClick={handleClick}>Select...</div>
     {isOpen && <div>{renderedDropDown}</div>}
         </div>
 }
